82d220c469aa2ee1a46f3f06127d86cec7287469
Ven Nguyen <vanven08ctt3@gmail.com>
2016-10-26 19:53:32 +0700

    Milestone4 part 1

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".alphabetical" do
+    it "Return [] when there are no Product" do
+      expect(Product.alphabetical()).to eq([])
+    end
+
+    it "Return [a] when there is only one product" do
+      @product = Product.new(name:"a", description:"product a", price_vnd:10000, weight:90);
+      @product.save
+      expect(Product.alphabetical).to eq([@product])
+    end
+
+    it "return [a,b,c] after creating 3 products c,b,a" do
+      @productC = Product.new(name:"c", description:"product c", price_vnd:10000, weight:90);
+      @productC.save
+      @productB = Product.new(name:"b", description:"product b", price_vnd:10000, weight:90);
+      @productB.save
+      @productA = Product.new(name:"a", description:"product a", price_vnd:10000, weight:90);
+      @productA.save
+      expect(Product.alphabetical).to eq([@productA,@productB,@productC])
+    end
+  end
 end
